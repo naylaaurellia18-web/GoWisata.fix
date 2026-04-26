88634efece6874808e3c678baa98836f3c108f75
naylaaurellia18-web <naylaaurellia18@student.uns.ac.id>
2026-04-27 01:48:28 +0700

    update koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,16 +1,6 @@
-<?php
-// Mendefinisikan parameter koneksi database
-$host     = "localhost";
-$user     = "root";
-$password = "";
-$database = "gowisata2";
+$conn = @mysqli_connect("localhost", "root", "", "gowisata2");
 
-// Membuat koneksi ke MySQL menggunakan fungsi mysqli_connect
-$conn = mysqli_connect($host, $user, $password, $database);
-
-// Melakukan pengecekan apakah koneksi berhasil atau gagal
 if (!$conn) {
-    // Jika gagal, hentikan program dan tampilkan pesan error
-    die("Koneksi gagal: " . mysqli_connect_error());
+    echo "Sistem Online (Database belum terhubung ke Cloud)";
+    exit();
 }
-?>
