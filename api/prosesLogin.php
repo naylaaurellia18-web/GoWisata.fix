d4ddf10650ef18b7a55f6485e985505678527f9e
naylaaurellia18-web <naylaaurellia18@student.uns.ac.id>
2026-04-28 12:56:46 +0700

    coba

--- api/prosesLogin.php
+++ api/prosesLogin.php
@@ -28,46 +28,4 @@ if (isset($_POST['login'])) {
         echo "<script>alert('Username atau Password Salah!');</script>";
     }
 }
-?><?php
-session_start();
-include 'koneksi.php';
-
-// Cek apakah koneksi tersedia
-if (!$conn) {
-    die("Koneksi database gagal: " . mysqli_connect_error());
-}
-
-if (isset($_POST['login'])) {
-    // Ambil data dari form
-    $user = isset($_POST['username']) ? mysqli_real_escape_string($conn, $_POST['username']) : '';
-    $pass = isset($_POST['password']) ? $_POST['password'] : '';
-
-    if (!empty($user) && !empty($pass)) {
-        // Query mencari pengguna
-        $sql = "SELECT * FROM pengguna WHERE username='$user' AND password='$pass'";
-        $query = mysqli_query($conn, $sql);
-        
-        if (!$query) {
-            die("Query gagal: " . mysqli_error($conn));
-        }
-
-        if (mysqli_num_rows($query) > 0) {
-            $data = mysqli_fetch_assoc($query);
-            $_SESSION['role'] = $data['role'];
-            $_SESSION['user'] = $data['username'];
-            
-            // Redirect sesuai role
-            if ($data['role'] == "admin") {
-                header("Location: admin_dashboard.php");
-            } else {
-                header("Location: index.html");
-            }
-            exit();
-        } else {
-            echo "<script>alert('Username atau Password Salah!'); window.location='login.php';</script>";
-        }
-    } else {
-        echo "<script>alert('Isi semua kolom!'); window.location='login.php';</script>";
-    }
-}
 ?>
